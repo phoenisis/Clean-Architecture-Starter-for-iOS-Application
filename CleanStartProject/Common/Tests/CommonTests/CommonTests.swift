d0cf8033efe42602a7ea529ab639da6b75bd7f8a
Quentin PIDOUX <quentin.pidoux@gmail.com>
2020-07-22 22:29:47 +0200

    fix project

--- CleanStartProject/Common/Tests/CommonTests/CommonTests.swift
+++ CleanStartProject/Common/Tests/CommonTests/CommonTests.swift
@@ -10,6 +10,6 @@ final class CommonTests: XCTestCase {
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testExample", testExample)
     ]
 }
--- CleanStartProject/Common/Tests/CommonTests/XCTestManifests.swift
+++ CleanStartProject/Common/Tests/CommonTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(CommonTests.allTests),
+        testCase(CommonTests.allTests)
     ]
 }
 #endif
